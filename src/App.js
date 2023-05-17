8e2fc03d509b5cf22bed9b686b69f3b0b49b8635
Str4nge1 <aleqsandre.b21@gmail.com>
2023-05-17 19:07:04 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useMemo, memo, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
 
-function App() {
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+const CustomCell = (props) => {
+  const { value, eGridCell } = props;
+  eGridCell.style.backgroundColor = `rgba(255, 0, 0, ${+value + 0.05})`;
+  return value;
+};
+
+const removeStyles = async function () {
+  const links = document.getElementsByTagName("link");
+  while (links[0]) links[0].parentNode.removeChild(links[0]);
+};
+
+const loadStylesheet = function (link) {
+  if (!link) return;
+
+  const linkElement = document.createElement("link");
+
+  linkElement.setAttribute("rel", "stylesheet");
+  linkElement.setAttribute("href", link);
+
+  document.getElementsByTagName("head")[0].appendChild(linkElement);
+};
+
+const App = ({ data, config, queryResponse }) => {
+  const gridRef = useRef();
+  const { showRowNumbers, customTheme, loadCustomTheme, limitRows } = config;
+  const { pivots, fields } = queryResponse;
+  const { dimensions, measures } = fields;
+
+  const [rowData, setRowData] = useState(() =>
+    data.map((el) => {
+      const rowDataObj = {
+        ...Object.fromEntries(
+          Object.entries(el[measures[0].name]).map(([key, el]) => {
+            return [key, Number.parseFloat(el.value).toFixed(2)];
+          })
+        ),
+      };
+
+      dimensions.forEach((dimension) => {
+        rowDataObj[dimension.field_group_variant] = el[dimension.name].value;
+      });
+
+      return rowDataObj;
+    })
+  );
+
+  console.log({ rowData, pivots });
+
+  const [columnDefs, setColumnDefs] = useState(() => [
+    {
+      headerName: "",
+      valueGetter: "node.rowIndex + 1",
+      field: "Row ID",
+      floatingFilter: false,
+      filter: false,
+      hide: !showRowNumbers,
+      width: 20,
+    },
+    ...dimensions.map(({ field_group_variant }) => ({
+      field: field_group_variant,
+    })),
+    ...pivots.map(({ key }) => ({
+      field: key,
+      cellRenderer: memo(CustomCell),
+    })),
+  ]);
+
+  console.log({ columnDefs });
+
+  const defaultColDef = useMemo(
+    () => ({
+      sortable: true,
+      filter: true,
+      resizable: true,
+      filterParams: {
+        debounceMs: 1000,
+        buttons: ["apply", "clear", "cancel", "reset"],
+      },
+      floatingFilter: true,
+    }),
+    []
+  );
+
+  useEffect(() => {
+    if (loadCustomTheme) {
+      removeStyles();
+      loadStylesheet(customTheme);
+    } else {
+      removeStyles();
+    }
+  }, [loadCustomTheme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh" }}>
+      <div className="ag-theme-alpine" style={{ height: "100%" }}>
+        <AgGridReact
+          ref={gridRef}
+          rowSelection="multiple"
+          rowData={limitRows ? rowData.slice(0, limitRows) : rowData}
+          columnDefs={columnDefs}
+          animateRows={true}
+          defaultColDef={defaultColDef}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,103 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import data from "./data.json";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const tableOptions = {
+  showRowNumbers: {
+    section: "Plot",
+    type: "boolean",
+    label: "Show Row Number",
+    default: false,
+    order: 0,
+  },
+  limitDisplayedRows: {
+    section: "Plot",
+    type: "boolean",
+    label: "Limit Displayed Rows",
+    default: false,
+    order: 1,
+  },
+  showFullFieldName: {
+    section: "Series",
+    type: "boolean",
+    label: "Show Full Field Name",
+    default: false,
+    order: 1,
+  },
+  customTheme: {
+    section: "Theme",
+    type: "string",
+    label: "Load custom CSS from:",
+    default: "",
+    order: 2,
+  },
+  loadCustomTheme: {
+    section: "Theme",
+    type: "boolean",
+    label: "Load",
+    default: false,
+    order: 3,
+  },
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function getConfigOptions(config) {
+  const newOptions = tableOptions;
+
+  if (config.limitDisplayedRows) {
+    newOptions["limitRows"] = {
+      section: "Plot",
+      type: "string",
+      label: "Limit Rows",
+      order: 2,
+    };
+  }
+
+  return newOptions;
+}
+
+/*eslint-disable no-undef */
+looker.plugins.visualizations.add({
+  id: "react_test",
+  label: "React Test",
+  create: function (element, config) {
+    element.innerHTML = `
+      <style>
+        
+      </style>
+    `;
+
+    let container = element.appendChild(document.createElement("div"));
+    container.className = "table-vis";
+    container.id = "vis-container";
+  },
+  updateAsync: function (data, element, config, queryResponse, details, done) {
+    this.clearErrors();
+
+    if (queryResponse.fields.dimensions.length == 0) {
+      this.addError({
+        title: "No Dimensions",
+        message: "This chart requires dimensions.",
+      });
+      return;
+    }
+
+    console.log({ data, config, queryResponse });
+
+    this.trigger("registerOptions", getConfigOptions(config));
+    const root = ReactDOM.createRoot(document.getElementById("vis-container"));
+    root.render(
+      <App data={data} config={config} queryResponse={queryResponse} />
+    );
+
+    done();
+  },
+});
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App data={data} />
+//   </React.StrictMode>
+// );
